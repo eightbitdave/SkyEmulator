37b34155a7246289f26e2400001ae578e8517dad
Galloway, John (Associate Software Developer) <john.galloway@sky.uk>
2015-10-15 16:55:38 +0100

    Email api working using Gmail SMTP. Needs templates sorting.

--- app/mailers/summary_mailer.rb
+++ app/mailers/summary_mailer.rb
@@ -1,2 +1,7 @@
 class SummaryMailer < ApplicationMailer
+  default from: 'ronan.spoor@sky.uk'
+
+  def test_email
+    mail(to: 'ronanm@gmail.com', subject: 'test foobar')
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,9 +3,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    adderss: "smtp.gmail.com",
+    address: "smtp.gmail.com",
     port: 587,
-    domain: "emulatorproject",
     authentication: "plain",
     user_name: ENV["EMAIL"],
     password: ENV["PASSWORD"],
